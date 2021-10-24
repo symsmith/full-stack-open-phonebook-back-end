7d6f551bf65a1c363fd3aa440850d30e9913c7fb
Syméon Smith <symeon.smith@gmail.com>
2021-10-24 14:24:34 +0200

    Add info page (Ex. 3.2)

--- index.js
+++ index.js
@@ -2,31 +2,40 @@ const express = require("express")
 const app = express(express.json())
 
 const persons = [
-    {
-      "id": 1,
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": 2,
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": 3,
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": 4,
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122"
+  }
 ]
 
-app.get("/api/persons", (req, res) => {res.json(persons)})
+app.get("/info", (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+     <p>${new Date()}</p>`
+  )
+})
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log("Server running on http://localhost:" + PORT);
+  console.log("Server running on http://localhost:" + PORT)
 })
