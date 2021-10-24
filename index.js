5cd5e309bb82b41e7833ed4daff0c4350a849113
Syméon Smith <symeon.smith@gmail.com>
2021-10-24 14:36:59 +0200

    Add delete route (Ex. 3.4)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express(express.json())
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -42,6 +42,11 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+  persons = persons.filter((p) => p.id !== Number(req.params.id))
+  res.status(204).end()
 })
 
 const PORT = 3001
